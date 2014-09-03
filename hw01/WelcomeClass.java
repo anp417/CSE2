309432cef91d79a507eb8b972cc181e12c836002
anp417 <anp417@lehigh.edu>
2014-09-03 00:44:19 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//An Phung
+//hw01
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+//  print statements
+    
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("| -A--N--P--4--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  v  v  v  v  v"); 
+    System.out.println("I am a sophomore majoring in Industrial & Systems Engineering. I like going to the beach, and smizing.");
+/* every \ needs to be double to avoid the illegal characters
+   it will still print as a regular \ */
+    }
+}
